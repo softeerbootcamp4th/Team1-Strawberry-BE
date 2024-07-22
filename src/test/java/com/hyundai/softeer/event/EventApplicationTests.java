095769a393d202125252bd278ac92946c8c3f5f2
min9805 <min1925k@gmail.com>
2024-07-22 15:27:52 +0900

    Initial Commit

---
+++ src/test/java/com/hyundai/softeer/event/EventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyundai.softeer.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
