8a9dd16e9cea263c29b44af0f4623812af6eb06e
min9805 <min1925k@gmail.com>
2024-07-23 14:41:16 +0900

    #2 feat: Jwt Filter 등록
    - API 요청 시 JWT 토큰 확인 필터 설정

--- src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
+++ src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
@@ -1,6 +1,12 @@
 package com.hyundai.softeer.backend.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyundai.softeer.backend.domain.user.service.UserService;
+import com.hyundai.softeer.backend.global.authentication.infra.TokenProvider;
+import com.hyundai.softeer.backend.global.filter.JwtAuthorizationFilter;
+import jakarta.servlet.Filter;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,5 +21,13 @@ public class WebConfig {
                 .setConnectTimeout(Duration.ofSeconds(5))
                 .setReadTimeout(Duration.ofSeconds(5))
                 .build();
+    }
+
+    @Bean
+    public FilterRegistrationBean jwtAuthorizationFilter(TokenProvider provider, ObjectMapper mapper) {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new JwtAuthorizationFilter(provider,mapper));
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
     }
 }
