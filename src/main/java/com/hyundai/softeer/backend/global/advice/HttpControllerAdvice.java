9501c0dbcd2714225d3cf5830b73acd7790498e0
mjmj <violet_prog@naver.com>
2024-07-30 17:50:11 +0900

    feat: Json 파싱 예외 시 에러 발생

--- src/main/java/com/hyundai/softeer/backend/global/advice/HttpControllerAdvice.java
+++ src/main/java/com/hyundai/softeer/backend/global/advice/HttpControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.hyundai.softeer.backend.global.advice;
 
+import com.hyundai.softeer.backend.global.exception.JsonParseException;
 import com.hyundai.softeer.backend.global.exception.NoContentException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,5 +14,11 @@ public class HttpControllerAdvice {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> handleNoContentException(NoContentException ex) {
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(JsonParseException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Void> jsonParseException(NoContentException ex) {
+        return ResponseEntity.internalServerError().build();
     }
 }
