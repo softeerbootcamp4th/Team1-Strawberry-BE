4a6e14ff1c1ff5ce38896c02e5af2c6064683d3b
min9805 <min1925k@gmail.com>
2024-07-31 17:33:26 +0900

    feat: Resolver 등록 (#45)

--- src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
+++ src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
@@ -1,19 +1,32 @@
 package com.hyundai.softeer.backend.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hyundai.softeer.backend.global.jwt.provider.TokenProvider;
 import com.hyundai.softeer.backend.global.filter.JwtAuthorizationFilter;
+import com.hyundai.softeer.backend.global.jwt.CurrentUserMethodArgumentResolver;
+import com.hyundai.softeer.backend.global.jwt.provider.TokenProvider;
 import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
+import java.util.List;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private CurrentUserMethodArgumentResolver currentUserMethodArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(currentUserMethodArgumentResolver);
+    }
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
@@ -25,7 +38,7 @@ public class WebConfig {
     @Bean
     public FilterRegistrationBean jwtAuthorizationFilter(TokenProvider provider, ObjectMapper mapper) {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new JwtAuthorizationFilter(provider,mapper));
+        filterRegistrationBean.setFilter(new JwtAuthorizationFilter(provider, mapper));
         filterRegistrationBean.setOrder(1);
         return filterRegistrationBean;
     }
