3308452cbd914dd548083f395917ee52091cf949
min9805 <56664567+min9805@users.noreply.github.com>
2024-07-31 15:18:40 +0900

    로그인 API Swagger 정리 추가합니다. (#47)
    * feat: #41 AuthController Swagger API 스펙 정리 (#41)
    
    * feat: #41 UserInfoDto 불필요한 정보 제거 (#41)
    
    * feat: #41 TokenDto 예시 작성 (#41)

--- src/main/java/com/hyundai/softeer/backend/global/jwt/dto/TokenDto.java
+++ src/main/java/com/hyundai/softeer/backend/global/jwt/dto/TokenDto.java
@@ -1,16 +1,14 @@
 package com.hyundai.softeer.backend.global.jwt.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class TokenDto {
+    @Schema(description = "Access Token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     private String accessToken;
+    @Schema(description = "Refresh Token", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
     private String refreshToken;
-
-    @Builder
-    public TokenDto(String accessToken, String refreshToken){
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 }
