4c45531289b572a8d97f093e60667e8e13ce9171
min9805 <min1925k@gmail.com>
2024-08-01 17:50:56 +0900

    feat: Github Action 타겟 변경 (#54)

--- src/main/java/com/hyundai/softeer/backend/domain/user/controller/AuthController.java
+++ src/main/java/com/hyundai/softeer/backend/domain/user/controller/AuthController.java
@@ -14,7 +14,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
@@ -73,7 +76,7 @@ public class AuthController {
                 .build();
     }
 
-    @PostMapping("/hyundai")
+    @GetMapping("/hyundai")
     @Operation(summary = "현대자동차 로그인", description = """
             # 현대자동차 로그인
                 
