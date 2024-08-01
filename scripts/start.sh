e296d9d52394570a1ef5e668720deef7dcc47fd5
min9805 <min1925k@gmail.com>
2024-08-01 18:45:29 +0900

    feat: 배포 스크립트 수정 (#54)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ROOT_PATH="/home/ubuntu/backend"
-VERSION=$(grep -E '^version=' "$ROOT_PATH/gradle.properties" | cut -d'=' -f2)
+
+VERSION=$(grep "version=" "$ROOT_PATH/gradle.properties" | sed 's/version=//')
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
@@ -11,7 +12,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/backend-$VERSION.jar" "$JAR
+cp "$ROOT_PATH/build/libs/backend-$VERSION.jar" "$JAR"
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -Dspring.profiles.active=prod -jar $JAR > $APP_LOG 2> $ERROR_LOG &
