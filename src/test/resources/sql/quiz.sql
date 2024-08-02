9dc6a841a20b71f999d29e05547ba15c7f184d65
mjmj <violet_prog@naver.com>
2024-08-02 10:11:10 +0900

    fix: 테스트 용 sql 파일 통합 (subEvent.sql, quiz.sql, event.sql, car.sql) -> (integration.sql)

--- src/test/resources/sql/quiz.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Quizzes (sequence, sub_event_id, anchor, answer, hint, problem, winners, winners_meta, init_consonant)
-values (1, 1, "#sub1", "10.4", "10 근처", "산타페의 연비는?", 2, "{\"1\": [1, 1, \"img1.com\"], \"2\": [2, 2, \"img2.com\"], \"3\": [3, 3, \"img3.com\"]}", "ㅅㅈㅅ"),
-       (2, 2, "#sub2", "11.5", "11 근처", "산타페의 연비는?", 3, "{\"1\": [2, 1, \"img1.com\"], \"2\": [3, 2, \"img2.com\"], \"3\": [4, 3, \"img3.com\"]}", "ㅅㅇㅈㅇ"),
-       (3, 3, "#sub3", "12.5", "12 근처", "산타페의 연비는?", 12, "{\"1\": [3, 1, \"img1.com\"], \"2\": [4, 2, \"img2.com\"], \"3\": [5, 3, \"img3.com\"]}", "ㅅㅇㅈㅇ");
