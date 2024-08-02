93ead7d1b2e54c53186a2073098f5f13b849117a
mjmj <violet_prog@naver.com>
2024-08-02 18:52:45 +0900

    fix: Erd 수정에 따른 코드 수정 (prize - quiz 일대다 연결)

--- src/main/java/com/hyundai/softeer/backend/domain/firstcomeevent/quiz/entity/Quiz.java
+++ src/main/java/com/hyundai/softeer/backend/domain/firstcomeevent/quiz/entity/Quiz.java
@@ -1,6 +1,8 @@
 package com.hyundai.softeer.backend.domain.firstcomeevent.quiz.entity;
 
+import com.hyundai.softeer.backend.domain.prize.entity.Prize;
 import com.hyundai.softeer.backend.domain.subevent.entity.BaseSubEvent;
+import com.hyundai.softeer.backend.domain.subevent.entity.SubEvent;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,11 +28,16 @@ public class Quiz extends BaseSubEvent {
   private String answer;
 
   private String hint;
-
   /**
    * 추후 결정
    */
   private String anchor;
 
   private String initConsonant;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Prize prize;
+
+  private Integer winnerCount;
+
 }
