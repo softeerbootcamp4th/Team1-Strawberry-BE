6226a431000e945d62e200ee1216025f65b36a09
mjmj <violet_prog@naver.com>
2024-08-02 15:39:25 +0900

    fix: erd 수정으로 인한 코드 수정 (Quizzes 테이블에서 winners_meta 컬럼 삭제)

--- src/main/java/com/hyundai/softeer/backend/domain/subevent/entity/BaseSubEvent.java
+++ src/main/java/com/hyundai/softeer/backend/domain/subevent/entity/BaseSubEvent.java
@@ -18,7 +18,6 @@ public class BaseSubEvent {
     @Column(name = "sub_event_id", nullable = false)
     private Long subEventId;
 
-    private String winners_meta;
 
     private Integer winners;
 }
