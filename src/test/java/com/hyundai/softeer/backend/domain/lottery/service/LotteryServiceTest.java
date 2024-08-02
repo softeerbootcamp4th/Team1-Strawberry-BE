154829eb9cf526fbf49e9bc27e64ed78662c16d5
mjmj <violet_prog@naver.com>
2024-08-02 18:35:55 +0900

    fix: WinnerInfo 객체 변경으로 인한 코드 수정

--- src/test/java/com/hyundai/softeer/backend/domain/lottery/service/LotteryServiceTest.java
+++ src/test/java/com/hyundai/softeer/backend/domain/lottery/service/LotteryServiceTest.java
@@ -34,7 +34,7 @@ class LotteryServiceTest {
                 EventUser.builder().id(5L).build());
 
         Map<Integer, WinnerInfo> winnersMeta = Map.of(
-                1, new WinnerInfo(3, 1));
+               1, new WinnerInfo(3, 1L, 1));
         LotteryScoreWeight scoreWeight = new LotteryScoreWeight(1.0, 1.0, 1.0, 1.0);
 
         // when
