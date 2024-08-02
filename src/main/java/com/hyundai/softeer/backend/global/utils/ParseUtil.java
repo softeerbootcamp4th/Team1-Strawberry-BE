ff3efb6932b938e08c1838206afe13d0abd2ee3f
mjmj <violet_prog@naver.com>
2024-08-02 18:45:13 +0900

    fix: WinnerInfo 변경을 통한 코드 수정

--- src/main/java/com/hyundai/softeer/backend/global/utils/ParseUtil.java
+++ src/main/java/com/hyundai/softeer/backend/global/utils/ParseUtil.java
@@ -3,6 +3,7 @@ package com.hyundai.softeer.backend.global.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hyundai.softeer.backend.domain.firstcomeevent.quiz.exception.JsonParseException;
 import com.hyundai.softeer.backend.domain.subevent.dto.WinnerInfo;
 
 import java.util.HashMap;
@@ -10,12 +11,18 @@ import java.util.Map;
 
 public class ParseUtil {
 
-    public static Map<Integer, WinnerInfo> parseWinnersMeta(String winnersMeta) throws JsonProcessingException {
+    public static Map<Integer, WinnerInfo> parseWinnersMeta(String winnersMeta) {
         Map<Integer, WinnerInfo> resultMap = new HashMap<>();
         ObjectMapper objectMapper = new ObjectMapper();
 
+        JsonNode rootNode;
+
         // JSON 문자열을 JsonNode로 변환
-        JsonNode rootNode = objectMapper.readTree(winnersMeta.replace("'", "\""));
+        try {
+            rootNode = objectMapper.readTree(winnersMeta.replace("'", "\""));
+        } catch (JsonProcessingException e) {
+            throw new JsonParseException();
+        }
 
         // 각 키와 값 파싱
         rootNode.fields().forEachRemaining(entry -> {
@@ -23,10 +30,10 @@ public class ParseUtil {
             JsonNode values = entry.getValue();
 
             int winnerCount = values.get(0).asInt();
-            int prizeId = values.get(1).asInt();
-            String prizeImg = values.get(2).asText();
+            long prizeId = values.get(1).asLong();
+            int _rank = values.get(2).asInt();
 
-            resultMap.put(rank, new WinnerInfo(winnerCount, prizeId, prizeImg));
+            resultMap.put(rank, new WinnerInfo(winnerCount, prizeId, _rank));
         });
         return resultMap;
     }
