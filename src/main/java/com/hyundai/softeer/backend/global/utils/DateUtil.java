181aa165c4c5dfd503b21fb4bb0240b911dec704
mjmj <violet_prog@naver.com>
2024-08-04 02:40:10 +0900

    refactoring: QuizService의 메소드인 (eventNotWithinPeriod, findClosestSubEvent)를 별도의 유틸 클래스로 분리했어요

--- src/main/java/com/hyundai/softeer/backend/global/utils/DateUtil.java
+++ src/main/java/com/hyundai/softeer/backend/global/utils/DateUtil.java
@@ -1,6 +1,8 @@
 package com.hyundai.softeer.backend.global.utils;
 
+import com.hyundai.softeer.backend.domain.event.entity.Event;
 import com.hyundai.softeer.backend.domain.subevent.entity.SubEvent;
+import com.hyundai.softeer.backend.domain.subevent.enums.SubEventType;
 import com.hyundai.softeer.backend.domain.subevent.exception.SubEventNotFoundException;
 import com.hyundai.softeer.backend.domain.subevent.repository.SubEventRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -35,5 +38,27 @@ public class DateUtil {
         LocalDateTime startAt = subEvent.getStartAt();
         Duration duration = Duration.between(current, startAt);
         return Math.max(0, duration.getSeconds());
+    }
+
+    public boolean eventNotWithinPeriod(Event event) {
+        LocalDateTime current = LocalDateTime.now(clock);
+        return current.isBefore(event.getStartAt()) || current.isAfter(event.getEndAt());
+    }
+
+    public SubEvent findClosestSubEvent(List<SubEvent> subEvents) {
+        LocalDateTime current = LocalDateTime.now(clock);
+        SubEvent closetSubEvent = null;
+        for(SubEvent subEvent: subEvents) {
+            if(subEvent.getEventType().equals(SubEventType.DRAWING)) continue;
+
+            if(isWithinSubEventPeriod(subEvent)) {
+                return subEvent;
+            }
+
+            if(current.isBefore(subEvent.getEndAt())) {
+                return subEvent;
+            }
+        }
+        return closetSubEvent;
     }
 }
