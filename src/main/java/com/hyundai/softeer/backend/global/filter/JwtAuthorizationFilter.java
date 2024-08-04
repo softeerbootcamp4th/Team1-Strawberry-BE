0cdf370ea44476f00dadfddf151c6d9d31191ed4
mjmj <violet_prog@naver.com>
2024-08-04 11:55:09 +0900

    feat: /api/v1/quiz, /api/v1/quiz/land를 인증 화이트리스트에 추가

--- src/main/java/com/hyundai/softeer/backend/global/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/hyundai/softeer/backend/global/filter/JwtAuthorizationFilter.java
@@ -27,7 +27,10 @@ public class JwtAuthorizationFilter implements Filter {
             "/api/v1/oauth2/**",
             "/swagger-ui/**",
             "/v3/api-docs",
-            "/v3/api-docs/**"};
+            "/v3/api-docs/**",
+            "/api/v1/quiz",
+            "/api/v1/quiz/land"
+    };
 
     private final TokenProvider jwtProvider;
 
