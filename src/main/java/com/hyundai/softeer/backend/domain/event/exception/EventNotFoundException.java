fecbbcc1760f962a7f42e088a9b197eb88135d1c
mjmj <violet_prog@naver.com>
2024-08-05 23:54:46 +0900

    fix: 일관성을 위해 NotExistEventException.java에서 EventNotFoundException으로 수정

--- src/main/java/com/hyundai/softeer/backend/domain/event/exception/EventNotFoundException.java
+++ src/main/java/com/hyundai/softeer/backend/domain/event/exception/EventNotFoundException.java
@@ -3,8 +3,8 @@ package com.hyundai.softeer.backend.domain.event.exception;
 import com.hyundai.softeer.backend.global.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class NotExistEventException extends BaseException {
-    public NotExistEventException() {
+public class EventNotFoundException extends BaseException {
+    public EventNotFoundException() {
         super(HttpStatus.NO_CONTENT, "존재하지 않는 이벤트 입니다.");
     }
 }
