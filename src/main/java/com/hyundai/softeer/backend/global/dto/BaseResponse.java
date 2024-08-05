10ae105d003bfb194d7e467295671e84d1893589
mjmj <108040422+violet-mj@users.noreply.github.com>
2024-08-05 10:38:20 +0900

    quiz api 문서 고도화 및 BaseResponse 적용 (#67)
    * feat: BaseResponse 객체의 description 추가
    
    * feat: /api/v1/quiz, /api/v1/quiz/land를 인증 화이트리스트에 추가
    
    * feat: QuizNotFoundException에 대한 ControllerAdvice 추가
    
    * feat: swagger generic resolver 옵션 추가
    
    * chore: 퀴즈 이벤트 api 문서 고도화 및 BaseResponse로 wrapping

--- src/main/java/com/hyundai/softeer/backend/global/dto/BaseResponse.java
+++ src/main/java/com/hyundai/softeer/backend/global/dto/BaseResponse.java
@@ -1,6 +1,7 @@
 package com.hyundai.softeer.backend.global.dto;
 
 import com.hyundai.softeer.backend.global.status.BaseResponseStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,10 +13,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BaseResponse<T> {
     @Builder.Default
+    @Schema(description = "응답 코드")
     private int status = BaseResponseStatus.SUCCESS.getStatus();
 
     @Builder.Default
+    @Schema(description = "응답 메시지")
     private String message = BaseResponseStatus.SUCCESS.getMessage();
 
+    @Schema(description = "응답 데이터")
     private T data;
 }
