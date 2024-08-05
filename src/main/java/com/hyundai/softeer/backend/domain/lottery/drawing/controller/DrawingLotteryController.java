80aade06babfa0fd22e6a7455af454240a0556da
min9805 <min1925k@gmail.com>
2024-08-05 18:59:20 +0900

    feat: 랭킹 조회 API 구현 및 BaseResponse 생성자 default 값 추가

--- src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
+++ src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
@@ -2,6 +2,7 @@ package com.hyundai.softeer.backend.domain.lottery.drawing.controller;
 
 import com.hyundai.softeer.backend.domain.lottery.drawing.dto.DrawingLotteryLandResponseDto;
 import com.hyundai.softeer.backend.domain.lottery.drawing.service.DrawingLotteryService;
+import com.hyundai.softeer.backend.domain.lottery.dto.RankDto;
 import com.hyundai.softeer.backend.domain.user.entity.User;
 import com.hyundai.softeer.backend.global.dto.BaseResponse;
 import com.hyundai.softeer.backend.global.jwt.CurrentUser;
@@ -21,6 +22,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -53,5 +56,27 @@ public class DrawingLotteryController {
             @RequestParam("eventId") Long eventId
     ) {
         return new BaseResponse<>(drawingLotteryService.getDrawingLotteryLand(user, eventId));
+    }
+
+    @GetMapping("/rank")
+    @Operation(summary = "드로잉 추첨 이벤트 랭킹 조회", description = """
+            # 드로잉 추첨 이벤트 랭킹 조회
+                        
+            - 드로잉 추첨 이벤트의 랭킹을 조회합니다.
+                        
+            ## 응답
+                        
+            - 조회 성공 시 `200` 코드와 랜딩 페이지 정보를 반환합니다.
+            - 이벤트 번호에 오류가 있을 경우 `404` 에러를 반환합니다.
+             
+            """)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "드로잉 추첨 이벤트 랭킹 조회 성공", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 이벤트 정보", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BaseResponse.class), examples = @ExampleObject("{\"message\":\"드로잉 이벤트가 존재하지 않습니다.\",\"status\":404}"))}),
+    })
+    public BaseResponse<List<RankDto>> getRankList(
+            @RequestParam("subEventId") Long subEventId
+    ) {
+        return new BaseResponse<>(drawingLotteryService.getRankList(subEventId, 3));
     }
 }
