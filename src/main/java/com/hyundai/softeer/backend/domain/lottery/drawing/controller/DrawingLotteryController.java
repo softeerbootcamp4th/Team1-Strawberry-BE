30c8accaca29697a001c87e27f1ba91e37317c9f
min9805 <min1925k@gmail.com>
2024-08-06 17:27:54 +0900

    feat: 추첨 메서드를 위한 API 생성 및 인가를 위한 ApiKey 임시 설정 (#37)

--- src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
+++ src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
@@ -1,9 +1,13 @@
 package com.hyundai.softeer.backend.domain.lottery.drawing.controller;
 
+import com.hyundai.softeer.backend.domain.event.dto.ApiKeyRequest;
+import com.hyundai.softeer.backend.domain.event.dto.EventRequest;
+import com.hyundai.softeer.backend.domain.event.service.EventService;
 import com.hyundai.softeer.backend.domain.lottery.drawing.dto.DrawingLotteryLandDto;
 import com.hyundai.softeer.backend.domain.lottery.drawing.service.DrawingLotteryService;
 import com.hyundai.softeer.backend.domain.lottery.dto.RankDto;
-import com.hyundai.softeer.backend.domain.user.entity.User;
+import com.hyundai.softeer.backend.domain.subevent.dto.SubEventRequest;
+import com.hyundai.softeer.backend.domain.winner.entity.Winner;
 import com.hyundai.softeer.backend.global.dto.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -17,7 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,6 +33,7 @@ import java.util.List;
 public class DrawingLotteryController {
     public static final int RANK_COUNT = 20;
     private final DrawingLotteryService drawingLotteryService;
+    private final EventService eventService;
 
     @GetMapping("/land")
     @Operation(summary = "드로잉 추첨 이벤트 랜딩 페이지 조회", description = """
@@ -48,9 +52,9 @@ public class DrawingLotteryController {
             @ApiResponse(responseCode = "404", description = "존재하지 않는 이벤트 정보", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BaseResponse.class), examples = @ExampleObject("{\"message\":\"드로잉 이벤트가 존재하지 않습니다.\",\"status\":404}"))}),
     })
     public BaseResponse<DrawingLotteryLandDto> getDrawingLandingPage(
-            @RequestParam("eventId") Long eventId
+            EventRequest eventRequest
     ) {
-        return new BaseResponse<>(drawingLotteryService.getDrawingLotteryLand(eventId));
+        return new BaseResponse<>(drawingLotteryService.getDrawingLotteryLand(eventRequest.getEventId()));
     }
 
     @GetMapping("/rank")
@@ -70,8 +74,51 @@ public class DrawingLotteryController {
             @ApiResponse(responseCode = "404", description = "존재하지 않는 이벤트 정보", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BaseResponse.class), examples = @ExampleObject("{\"message\":\"드로잉 이벤트가 존재하지 않습니다.\",\"status\":404}"))}),
     })
     public BaseResponse<List<RankDto>> getRankList(
-            @RequestParam("subEventId") Long subEventId
+            SubEventRequest subEventRequest
     ) {
-        return new BaseResponse<>(drawingLotteryService.getRankList(subEventId, RANK_COUNT));
+        return new BaseResponse<>(drawingLotteryService.getRankList(subEventRequest.getSubEventId(), RANK_COUNT));
+    }
+
+    @GetMapping("/winner")
+    @Operation(summary = "드로잉 추첨 이벤트 당첨자 조회", description = """
+            # 드로잉 추첨 이벤트 당첨자 조회
+                        
+            - 드로잉 추첨 이벤트의 당첨자를 조회합니다.
+                        
+            ## 응답
+                        
+            - 조회 성공 시 `200` 코드와 랜딩 페이지 정보를 반환합니다.
+            - 이벤트 번호에 오류가 있을 경우 `404` 에러를 반환합니다.
+             
+            """)
+    @ApiResponses(value = {})
+    public BaseResponse<List<RankDto>> getWinnerList(
+            SubEventRequest subEventRequest
+    ) {
+        return null;
+    }
+
+    @GetMapping("/draw")
+    @Operation(summary = "드로잉 추첨 이벤트 추첨", description = """
+            # 드로잉 추첨 이벤트 추첨
+                        
+            - 드로잉 추첨 이벤트의 당첨자를 추첨합니다.
+                        
+            ## 응답
+                        
+            - 조회 성공 시 `200` 코드와 랜딩 페이지 정보를 반환합니다.
+            - 이벤트 번호에 오류가 있을 경우 `404` 에러를 반환합니다.
+             
+            """)
+    @ApiResponses(value = {})
+    public BaseResponse<List<Winner>> drawWinner(
+            SubEventRequest subEventRequest,
+            ApiKeyRequest apiKeyRequest
+    ) {
+        if (!eventService.validateApiKey(apiKeyRequest)) {
+            throw new IllegalArgumentException("Invalid API Key");
+        }
+
+        return new BaseResponse<>(drawingLotteryService.drawWinner(subEventRequest.getSubEventId()));
     }
 }
