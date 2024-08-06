4c333e1a8399965b4981107841a81c6a94c097c1
min9805 <min1925k@gmail.com>
2024-08-06 17:15:47 +0900

    feat: EventUser 데이터베이스에서 N 명 추첨 메서드 생성 (#37)

--- src/main/java/com/hyundai/softeer/backend/domain/eventuser/repository/EventUserRepository.java
+++ src/main/java/com/hyundai/softeer/backend/domain/eventuser/repository/EventUserRepository.java
@@ -9,9 +9,17 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface EventUserRepository extends JpaRepository<EventUser, Long> {
-    EventUser findByUserIdAndSubEventId(Long userId, Long subEventId);
+    EventUser findByUserIdAndSubEventId(long userId, long subEventId);
 
     @Query("SELECT new com.hyundai.softeer.backend.domain.lottery.dto.RankDto(u.name, eu.gameScore) FROM EventUser eu JOIN eu.user u WHERE eu.subEvent.id = :subEventId " +
             "ORDER BY eu.gameScore DESC")
-    List<RankDto> findTopNBySubEventId(Long subEventId, Pageable pageable);
+    List<RankDto> findTopNBySubEventId(long subEventId, Pageable pageable);
+
+    @Query("SELECT eu FROM EventUser eu " +
+            "WHERE eu.subEvent.id = :subEventId AND eu.id >= :rand")
+    List<EventUser> findNByRand(long subEventId, int rand, Pageable pageable);
+
+    @Query("SELECT eu FROM EventUser eu " +
+            "WHERE eu.subEvent.id = :subEventId")
+    List<EventUser> findRestByRand(long subEventId, Pageable pageable);
 }
