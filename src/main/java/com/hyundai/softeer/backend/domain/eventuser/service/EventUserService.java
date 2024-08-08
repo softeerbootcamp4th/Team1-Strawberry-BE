37bdd16ce877ba3571a9f29fba17bdcc7ea39648
min9805 <min1925k@gmail.com>
2024-08-08 16:11:22 +0900

    feat: 이벤트 참가 유저 랜덤값 생성 및 예외 처리 (#100)

--- src/main/java/com/hyundai/softeer/backend/domain/eventuser/service/EventUserService.java
+++ src/main/java/com/hyundai/softeer/backend/domain/eventuser/service/EventUserService.java
@@ -1,17 +1,21 @@
 package com.hyundai.softeer.backend.domain.eventuser.service;
 
 import com.hyundai.softeer.backend.domain.eventuser.dto.EventUserInfoDto;
+import com.hyundai.softeer.backend.domain.eventuser.dto.SharedUrlDto;
 import com.hyundai.softeer.backend.domain.eventuser.entity.EventUser;
 import com.hyundai.softeer.backend.domain.eventuser.exception.EventUserNotFoundException;
+import com.hyundai.softeer.backend.domain.eventuser.exception.NonPlayedUserException;
 import com.hyundai.softeer.backend.domain.eventuser.repository.EventUserRepository;
 import com.hyundai.softeer.backend.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class EventUserService {
+    public static final int SHARED_URL_LENGTH = 6;
     private final EventUserRepository eventUserRepository;
 
     @Transactional(readOnly = true)
@@ -20,5 +24,24 @@ public class EventUserService {
                 .orElseThrow(() -> new EventUserNotFoundException());
 
         return EventUserInfoDto.fromEntity(eventUser);
+    }
+
+    public SharedUrlDto getSharedUrl(User user, long subEventId) {
+
+        EventUser eventUser = eventUserRepository.findByUserIdAndSubEventId(user.getId(), subEventId)
+                .orElseThrow(() -> new EventUserNotFoundException());
+
+        String sharedUrl = eventUser.getSharedUrl();
+        if (sharedUrl.isBlank()) {
+            sharedUrl = RandomStringUtils.random(SHARED_URL_LENGTH, true, true);
+        }
+
+        double gameScore = eventUser.getGameScore();
+
+        if (gameScore <= 0.0) {
+            throw new NonPlayedUserException();
+        }
+
+        return new SharedUrlDto(sharedUrl, gameScore);
     }
 }
