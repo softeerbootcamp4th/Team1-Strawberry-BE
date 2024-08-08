874a736c08a0c903b0ce3bbba5c3be72ce71513e
mjmj <violet_prog@naver.com>
2024-08-08 19:04:33 +0900

    fix: POST 메서드에 @ParameterObject 제거

--- src/main/java/com/hyundai/softeer/backend/domain/expectation/dto/ExpectationRegisterRequest.java
+++ src/main/java/com/hyundai/softeer/backend/domain/expectation/dto/ExpectationRegisterRequest.java
@@ -1,18 +1,13 @@
 package com.hyundai.softeer.backend.domain.expectation.dto;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.springdoc.core.annotations.ParameterObject;
 
 @Data
 @NoArgsConstructor
-@ParameterObject
 public class ExpectationRegisterRequest {
 
-    @Parameter
     @NotBlank
     @Size(max=300)
     private String comment;
