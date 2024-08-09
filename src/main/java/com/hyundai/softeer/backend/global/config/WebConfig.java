34bb96bb73bffe6165f59579142bcf862144be3a
min9805 <min1925k@gmail.com>
2024-08-09 11:36:13 +0900

    feat: cors 와이드카드 설정

--- src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
+++ src/main/java/com/hyundai/softeer/backend/global/config/WebConfig.java
@@ -54,7 +54,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(corsOrigin)
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
