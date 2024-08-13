1cc12fe818187e5308407965d13a0ab3f4aaad97
Mingyu <min1925k@gmail.com>
2024-08-13 19:16:16 +0900

    feat: get 사용 로직 메서드 분리 (#124)

--- src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/service/ScoreCalculator.java
+++ src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/service/ScoreCalculator.java
@@ -17,7 +17,7 @@ public class ScoreCalculator {
 
             // 각 answerPoint에 대해 userPoints의 최소 거리 계산
             for (PositionDto userPoint : userPoints) {
-                double distance = Math.sqrt(Math.pow(answerPoint.getX() - userPoint.getX(), 2) + Math.pow(answerPoint.getY() - userPoint.getY(), 2));
+                double distance = getDistance(answerPoint, userPoint);
                 minDistance = Math.min(minDistance, distance);
             }
 
@@ -38,6 +38,11 @@ public class ScoreCalculator {
         return count == 0 ? 0 : totalScore / count; // count가 0일 경우 0 반환
     }
 
+    private double getDistance(PositionDto answerPoint, PositionDto userPoint) {
+        double distance = Math.sqrt(Math.pow(answerPoint.getX() - userPoint.getX(), 2) + Math.pow(answerPoint.getY() - userPoint.getY(), 2));
+        return distance;
+    }
+
     public double calculateAverageCosineSimilarity(List<PositionDto> userPoints, List<PositionDto> answerPoints) {
         double totalSimilarity = 0;
         int count = 0;
@@ -45,8 +50,7 @@ public class ScoreCalculator {
         for (PositionDto points : answerPoints) {
             double maxSimilarity = -1;
             for (PositionDto userPoint : userPoints) {
-                double similarity = (points.getX() * userPoint.getX() + points.getY() * userPoint.getY()) /
-                        (Math.sqrt(Math.pow(points.getX(), 2) + Math.pow(points.getY(), 2)) * Math.sqrt(Math.pow(userPoint.getX(), 2) + Math.pow(userPoint.getY(), 2)));
+                double similarity = getSimilarity(points, userPoint);
                 maxSimilarity = Math.max(maxSimilarity, similarity);
             }
             double scaledScore = ((maxSimilarity + 1) / 2) * 100;
@@ -55,5 +59,11 @@ public class ScoreCalculator {
         }
 
         return totalSimilarity / count;
+    }
+
+    private double getSimilarity(PositionDto points, PositionDto userPoint) {
+        double similarity = (points.getX() * userPoint.getX() + points.getY() * userPoint.getY()) /
+                (Math.sqrt(Math.pow(points.getX(), 2) + Math.pow(points.getY(), 2)) * Math.sqrt(Math.pow(userPoint.getX(), 2) + Math.pow(userPoint.getY(), 2)));
+        return similarity;
     }
 }
