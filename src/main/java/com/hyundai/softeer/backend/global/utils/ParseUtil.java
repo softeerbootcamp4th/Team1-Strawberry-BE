782f7a3966a278719bc9c8048d415fcafafd8309
Mingyu <min1925k@gmail.com>
2024-08-13 16:04:22 +0900

    feat: 정답 json 파싱을 위한 의존성 주입과 util 메서드 생성 (#124)

--- src/main/java/com/hyundai/softeer/backend/global/utils/ParseUtil.java
+++ src/main/java/com/hyundai/softeer/backend/global/utils/ParseUtil.java
@@ -1,8 +1,18 @@
 package com.hyundai.softeer.backend.global.utils;
 
+import com.hyundai.softeer.backend.domain.lottery.drawing.dto.PositionDto;
 import com.hyundai.softeer.backend.domain.subevent.dto.WinnerInfo;
+import com.hyundai.softeer.backend.global.exception.error.JsonParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ParseUtil {
@@ -27,5 +37,33 @@ public class ParseUtil {
         }
 
         return winners;
+    }
+
+    public static List<PositionDto> parsePositionsFromJson(String jsonUrl) {
+        List<PositionDto> positions = new ArrayList<>();
+
+        // JSON 데이터 읽기
+        try (InputStream is = new URL(jsonUrl).openStream()) {
+            StringBuilder jsonBuilder = new StringBuilder();
+            int byteRead;
+            while ((byteRead = is.read()) != -1) {
+                jsonBuilder.append((char) byteRead);
+            }
+
+            // JSON 데이터 파싱
+            JSONArray jsonArray = new JSONArray(jsonBuilder.toString());
+
+            // Position 객체 리스트 생성
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject point = jsonArray.getJSONObject(i);
+                double x = point.getDouble("x");
+                double y = point.getDouble("y");
+                positions.add(new PositionDto(x, y));
+            }
+        } catch (IOException e) {
+            throw new JsonParseException();
+        }
+
+        return positions;
     }
 }
--- src/test/java/com/hyundai/softeer/backend/global/utils/ParseUtilTest.java
+++ src/test/java/com/hyundai/softeer/backend/global/utils/ParseUtilTest.java
@@ -1,14 +1,19 @@
 package com.hyundai.softeer.backend.global.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.hyundai.softeer.backend.domain.lottery.drawing.dto.PositionDto;
 import com.hyundai.softeer.backend.domain.subevent.dto.WinnerInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class ParseUtilTest {
 
     @Test
@@ -29,5 +34,20 @@ class ParseUtilTest {
         assertThat(winnersMetaInfo.get(1)).isEqualTo(new WinnerInfo(1, 1L, 1));
         assertThat(winnersMetaInfo.get(2)).isEqualTo(new WinnerInfo(2, 2L, 2));
         assertThat(winnersMetaInfo.get(3)).isEqualTo(new WinnerInfo(3, 3L, 3));
+    }
+
+    @Test
+    @DisplayName("Json 파싱 테스트")
+    void jsonParsingTest() {
+        // given
+        String jsonUrl = "https://softeer-static.s3.ap-northeast-2.amazonaws.com/drawingLottery/answer/contour_points_01.json";
+
+        // when
+        List<PositionDto> positions = ParseUtil.parsePositionsFromJson(jsonUrl);
+
+        // then
+        assertThat(positions).isNotEmpty();
+
+        log.info("positions: {}", positions);
     }
 }
