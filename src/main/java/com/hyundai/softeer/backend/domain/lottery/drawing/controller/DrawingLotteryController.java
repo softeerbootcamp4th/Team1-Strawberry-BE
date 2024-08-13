555984e7c3faf09db20225512cec85d2d0f839dc
Mingyu <min1925k@gmail.com>
2024-08-13 20:29:50 +0900

    feat: 로그인 사용자만 API 허용 (#124)

--- src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
+++ src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/controller/DrawingLotteryController.java
@@ -93,14 +93,16 @@ public class DrawingLotteryController {
             - 조회 성공 시 `200` 코드와 게임 정보를 반환합니다.
             - 이벤트 번호에 오류가 있을 경우 `404` 에러를 반환합니다.             
             """)
+    @SecurityRequirement(name = "access-token")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "드로잉 추첨 이벤트 랭킹 조회 성공", useReturnTypeSchema = true),
             @ApiResponse(responseCode = "404", description = "존재하지 않는 이벤트 정보", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = BaseResponse.class), examples = @ExampleObject("{\"message\":\"드로잉 이벤트가 존재하지 않습니다.\",\"status\":404}"))}),
     })
     public BaseResponse<DrawingInfoDtos> getDrawingGameInfo(
+            @Parameter(hidden = true) @CurrentUser User authenticatedUser,
             @Validated SubEventRequest subEventRequest
     ) {
-        return new BaseResponse<>(drawingLotteryService.getDrawingGameInfo(subEventRequest));
+        return new BaseResponse<>(drawingLotteryService.getDrawingGameInfo(authenticatedUser, subEventRequest));
     }
 
     @PostMapping("")
