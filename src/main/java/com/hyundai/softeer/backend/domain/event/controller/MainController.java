6624e27b9e5fe8be9a03db14749fbfa5ebe2e046
mjmj <violet_prog@naver.com>
2024-08-14 16:11:53 +0900

    fix: swagger example이 부적절하게 나와있는 것을 확인하고 수정

--- src/main/java/com/hyundai/softeer/backend/domain/event/controller/MainController.java
+++ src/main/java/com/hyundai/softeer/backend/domain/event/controller/MainController.java
@@ -38,7 +38,36 @@ public class MainController {
             - 정상 반환 시 `200`이 반환됩니다.
             """)
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "정상 반환 시", useReturnTypeSchema = true),
+            @ApiResponse(responseCode = "200", description = "정상 반환 시", useReturnTypeSchema = true, content = {@Content(examples = @ExampleObject("{\n" +
+                    "    \"status\": 200,\n" +
+                    "    \"message\": \"OK\",\n" +
+                    "    \"data\": {\n" +
+                    "        \"remainSecond\": 0,\n" +
+                    "        \"imgs\": {\n" +
+                    "            \"mainImgUrl\": \"www.mainimg.com\",\n" +
+                    "            \"quizMainImg\": \"www.quizmain.com\",\n" +
+                    "            \"eventInfoImg\": \"www.eventinfo.com\",\n" +
+                    "            \"quizPrizeImg\": \"www.qprize.com\",\n" +
+                    "            \"drawingMainImg\": \"www.drawingmain.com\",\n" +
+                    "            \"scrolledImgUrl\": \"www.scroll.com\",\n" +
+                    "            \"drawingPrizeImg\": \"www.drawingprize.com\"\n" +
+                    "        },\n" +
+                    "        \"quizEventStartAt\": [\n" +
+                    "            {\n" +
+                    "                \"isStarted\": true,\n" +
+                    "                \"startAt\": \"2024-08-06T10:30:00\"\n" +
+                    "            },\n" +
+                    "            {\n" +
+                    "                \"isStarted\": true,\n" +
+                    "                \"startAt\": \"2024-08-12T10:30:00\"\n" +
+                    "            },\n" +
+                    "            {\n" +
+                    "                \"isStarted\": false,\n" +
+                    "                \"startAt\": \"2024-08-19T10:30:00\"\n" +
+                    "            }\n" +
+                    "        ]\n" +
+                    "    }\n" +
+                    "}"))}),
             @ApiResponse(responseCode = "404", description = "퀴즈x", content = {@Content(schema = @Schema(implementation = ApiErrorResponse.class), examples = @ExampleObject("{\"message\":\"퀴즈 이벤트가 존재하지 않습니다.\",\"status\":404}"))})
     })
     @GetMapping("/api/v1/land")
