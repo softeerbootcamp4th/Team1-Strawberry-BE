ff533f8c9ffcf502cad7297d5352ac78005dcb2e
mjmj <violet_prog@naver.com>
2024-08-14 13:39:23 +0900

    fix: 중복 검사가 있을 때 Dto 템플릿 추가

--- src/main/java/com/hyundai/softeer/backend/domain/firstcome/quiz/dto/QuizFirstComeSubmitResponseDto.java
+++ src/main/java/com/hyundai/softeer/backend/domain/firstcome/quiz/dto/QuizFirstComeSubmitResponseDto.java
@@ -10,17 +10,22 @@ import lombok.Getter;
 public class QuizFirstComeSubmitResponseDto {
     private Boolean isCorrect;
     private Boolean isWinner;
+    private Boolean isParticipant;
     private String prizeImgUrl;
 
     public static QuizFirstComeSubmitResponseDto winner(String prizeImgUrl) {
-        return new QuizFirstComeSubmitResponseDto(true, true, prizeImgUrl);
+        return new QuizFirstComeSubmitResponseDto(true, true, false, prizeImgUrl);
     }
 
     public static QuizFirstComeSubmitResponseDto correctBut() {
-        return new QuizFirstComeSubmitResponseDto(true, false, null);
+        return new QuizFirstComeSubmitResponseDto(true, false, false, null);
     }
 
     public static QuizFirstComeSubmitResponseDto notCorrect() {
-        return new QuizFirstComeSubmitResponseDto(false, false, null);
+        return new QuizFirstComeSubmitResponseDto(false, false, false, null);
+    }
+
+    public static QuizFirstComeSubmitResponseDto alreadyParticipant() {
+        return new QuizFirstComeSubmitResponseDto(false, false, true, null);
     }
 }
