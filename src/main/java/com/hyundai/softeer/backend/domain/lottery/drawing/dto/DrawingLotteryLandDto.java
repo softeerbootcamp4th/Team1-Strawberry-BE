f6660521deb86654f6f6ccd87dbe0deef23c9aca
Mingyu <min1925k@gmail.com>
2024-08-14 15:47:24 +0900

    feat: 랜딩 페이지 조회 시 subEvent 반환되도록 DTO 변경 (#128)

--- src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/dto/DrawingLotteryLandDto.java
+++ src/main/java/com/hyundai/softeer/backend/domain/lottery/drawing/dto/DrawingLotteryLandDto.java
@@ -14,7 +14,6 @@ public class DrawingLotteryLandDto {
     @Schema(description = "배너 이미지 url", example = "https://www.example.com/banner.jpg")
     private String bannerImgUrl;
 
-    //TODO : 파싱을 해서 주어야하는 지, 확인
     @Schema(description = "이벤트 이미지 url", example = "{ \"description_url\": \"https://www.example.com/description.jpg\", \"main_url\": \"https://www.example.com/main.jpg\" }")
     private Map<String, Object> eventImgUrls;
 
@@ -24,12 +23,16 @@ public class DrawingLotteryLandDto {
     @Schema(description = "이벤트 종료 시간", example = "2021-08-30T00:00:00")
     private LocalDateTime endAt;
 
+    @Schema(example = "4")
+    private long subEventId;
+
     public static DrawingLotteryLandDto fromEntity(SubEvent subEvent) {
         return DrawingLotteryLandDto.builder()
                 .bannerImgUrl(subEvent.getBannerImgUrl())
                 .eventImgUrls(subEvent.getEventImgUrls())
                 .startAt(subEvent.getStartAt())
                 .endAt(subEvent.getEndAt())
+                .subEventId(subEvent.getId())
                 .build();
     }
 }
