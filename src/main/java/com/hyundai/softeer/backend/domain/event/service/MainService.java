7122b111c95a5794b7d24c38be20248f44c1001a
mjmj <violet_prog@naver.com>
2024-08-14 16:01:36 +0900

    fix: MainLandDto 수정에 따른 로직 수정

--- src/main/java/com/hyundai/softeer/backend/domain/event/service/MainService.java
+++ src/main/java/com/hyundai/softeer/backend/domain/event/service/MainService.java
@@ -1,16 +1,19 @@
 package com.hyundai.softeer.backend.domain.event.service;
 
+import com.hyundai.softeer.backend.domain.event.dto.QuizStartInfo;
 import com.hyundai.softeer.backend.domain.event.entity.Event;
 import com.hyundai.softeer.backend.domain.event.exception.EventNotFoundException;
 import com.hyundai.softeer.backend.domain.event.repository.EventRepository;
 import com.hyundai.softeer.backend.domain.event.dto.MainLandDto;
 import com.hyundai.softeer.backend.domain.subevent.entity.SubEvent;
+import com.hyundai.softeer.backend.domain.subevent.enums.SubEventExecuteType;
 import com.hyundai.softeer.backend.domain.subevent.exception.SubEventNotFoundException;
 import com.hyundai.softeer.backend.domain.subevent.repository.SubEventRepository;
 import com.hyundai.softeer.backend.global.utils.DateUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -36,11 +39,26 @@ public class MainService {
            throw new SubEventNotFoundException();
         }
 
+        List<SubEvent> quizEvents = subEventRepository.findByEventIdAndExecuteType(eventId, SubEventExecuteType.FIRSTCOME);
+
+        if(quizEvents.isEmpty()) {
+            throw new EventNotFoundException();
+        }
+
+        List<QuizStartInfo> startAts = quizEvents.stream()
+                .map((quizEvent) -> {
+                    boolean isStarted = dateUtil.isSubEventStarted(quizEvent);
+                    LocalDateTime startAt = quizEvent.getStartAt();
+                    return new QuizStartInfo(isStarted, startAt);
+                })
+                .toList();
+
         int remainSecond = (int) dateUtil.startBetweenCurrentDiff(closestSubEvent);
 
         return MainLandDto.builder()
                 .imgs(eventImgUrls)
                 .remainSecond(remainSecond)
+                .quizEventStartAt(startAts)
                 .build();
     }
 }
