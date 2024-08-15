f3f0715097c2828cf1183e6640986edff871d1d1
mjmj <violet_prog@naver.com>
2024-08-15 19:20:29 +0900

    feat: findEventUserWithWinnerState 이벤트 참가자가 당첨되었는지 여부를 담고 있는 리스트 반환

--- src/main/java/com/hyundai/softeer/backend/domain/eventuser/repository/EventUserRepository.java
+++ src/main/java/com/hyundai/softeer/backend/domain/eventuser/repository/EventUserRepository.java
@@ -1,10 +1,12 @@
 package com.hyundai.softeer.backend.domain.eventuser.repository;
 
 import com.hyundai.softeer.backend.domain.eventuser.entity.EventUser;
+import com.hyundai.softeer.backend.domain.eventuser.projection.EventUserProjection;
 import com.hyundai.softeer.backend.domain.lottery.dto.RankDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +33,26 @@ public interface EventUserRepository extends JpaRepository<EventUser, Long> {
     @Query("SELECT MAX(eu.id) FROM EventUser eu WHERE eu.subEvent.id = :subEventId")
     long findMaxBySubEventId(Long subEventId);
 
+    @Query(value = "SELECT u.name as name, u.birth_date as birthDate, u.phone_number as phoneNumber, u.email as email, " +
+            "CASE WHEN SUM(CASE WHEN eu.is_winner THEN 1 ELSE 0 END) > 0 THEN 1 ELSE 0 END as isWinner "+
+            "FROM users u " +
+            "JOIN event_users eu ON u.id = eu.user_id " +
+            "WHERE eu.event_id = :eventId " +
+            "GROUP BY u.id, u.name, u.birth_date, u.phone_number, u.email " +
+            "ORDER BY u.id " +
+            "LIMIT :limit OFFSET :offset",
+            nativeQuery = true)
+    List<EventUserProjection> findEventUserWithWinnerState(@Param("eventId") Long eventId,
+                                                           @Param("offset") int offset,
+                                                           @Param("limit") int limit);
+
+    @Query(value = "SELECT COUNT(DISTINCT eu.user_id) " +
+            "FROM event_users eu " +
+            "WHERE eu.event_id = :eventId",
+            nativeQuery = true)
+    Integer countEventUserSize(@Param("eventId") Long eventId);
+
     Optional<EventUser> findBySharedUrl(String sharedUrl);
 
+    List<EventUser> findByEventId(Long eventId, Pageable pageable);
 }
