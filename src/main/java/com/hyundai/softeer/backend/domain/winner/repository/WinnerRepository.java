4c2f7f2fc27cca47a630a6c08abc890f3a166675
mjmj <violet_prog@naver.com>
2024-08-18 19:35:17 +0900

    fix: 여러 값이 나올 수 있으므로 Optional이 아닌 List로 수정

--- src/main/java/com/hyundai/softeer/backend/domain/winner/repository/WinnerRepository.java
+++ src/main/java/com/hyundai/softeer/backend/domain/winner/repository/WinnerRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 public interface WinnerRepository extends JpaRepository<Winner, Long> {
     Optional<List<Winner>> findBySubEventId(Long subEventId);
 
-    Optional<Winner> findByUserIdAndSubEventId(Long userId, Long subEventId);
+    List <Winner> findByUserIdAndSubEventId(Long userId, Long subEventId);
 }
