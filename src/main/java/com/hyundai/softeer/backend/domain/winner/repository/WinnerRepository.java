6edfd261c9fe098fc6c7e388a3f1f68fbed1c4f6
mjmj <violet_prog@naver.com>
2024-08-18 21:31:35 +0900

    feat: 당첨자 수를 세는 쿼리 작성

--- src/main/java/com/hyundai/softeer/backend/domain/winner/repository/WinnerRepository.java
+++ src/main/java/com/hyundai/softeer/backend/domain/winner/repository/WinnerRepository.java
@@ -2,6 +2,8 @@ package com.hyundai.softeer.backend.domain.winner.repository;
 
 import com.hyundai.softeer.backend.domain.winner.entity.Winner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,7 @@ public interface WinnerRepository extends JpaRepository<Winner, Long> {
     Optional<List<Winner>> findBySubEventId(Long subEventId);
 
     List <Winner> findByUserIdAndSubEventId(Long userId, Long subEventId);
+
+    @Query("SELECT COUNT(w) FROM Winner w WHERE w.subEvent.id = :subEventId")
+    long countWinnerBySubEventId(@Param("subEventId") Long subEventId);
 }
