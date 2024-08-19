183ec19ec79a582130e92fe41390363743618a55
mjmj <violet_prog@naver.com>
2024-08-19 16:05:20 +0900

    feat: 레디스 연결 및 테스트

---
+++ src/main/java/com/hyundai/softeer/backend/global/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.hyundai.softeer.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
