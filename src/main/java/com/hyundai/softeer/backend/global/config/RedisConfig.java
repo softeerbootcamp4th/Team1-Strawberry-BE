6d04a630dc1dd5dba2f879f3eeb856f452ba2b85
mjmj <violet_prog@naver.com>
2024-08-20 11:10:13 +0900

    fix: @Configuration 추가

--- src/main/java/com/hyundai/softeer/backend/global/config/RedisConfig.java
+++ src/main/java/com/hyundai/softeer/backend/global/config/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.hyundai.softeer.backend.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Configuration
 public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
