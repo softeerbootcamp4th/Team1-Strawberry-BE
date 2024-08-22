0899694027b9cf9d8856b49fdb5eaee42c2c7a2b
min9805 <min1925k@gmail.com>
2024-08-22 13:36:14 +0900

    feat: validate 추가

--- src/main/java/com/hyundai/softeer/backend/domain/firstcome/dto/WaitingEnqueueBodyRequest.java
+++ src/main/java/com/hyundai/softeer/backend/domain/firstcome/dto/WaitingEnqueueBodyRequest.java
@@ -1,10 +1,14 @@
 package com.hyundai.softeer.backend.domain.firstcome.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@NotNull
+@NoArgsConstructor
 public class WaitingEnqueueBodyRequest {
+    @NotNull
+    @Schema(example = "1")
     Long subEventId;
 }
